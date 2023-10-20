1b26acb61a9d6ef00a86b9384a1af7ad8641e29b
Patrick Wöhrer <patrickwhrer@gmail.com>
2023-10-20 21:58:45 +0200

    temperature and humidity added. Works.

--- TemperatureAndHumidity.ino
+++ TemperatureAndHumidity.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SimpleDHT.h>
 
+SimpleDHT11 dht11(26);
+
+void setup() {
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float temperature = 0;
+  float humidty = 0;
+  int err = SimpleDHTErrSuccess;
 
+  if((err=dht11.read2(&temperature, &humidty, NULL)) != SimpleDHTErrSuccess){
+    Serial.print("Read DHT11 failed, err=");
+    Serial.println(err);
+    delay(2000);
+    return;
+  }
+  Serial.println();
+  Serial.printf("Temp %.0f", temperature);
+  Serial.print("°C \n");
+  Serial.printf("Humidity %.0f", humidty);
+  Serial.printf("%%");
+  Serial.println(); 
+  delay(5000);
 }
