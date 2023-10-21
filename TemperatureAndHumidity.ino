4e3b78f5db16f1857c0f527bbb319128932ea07d
Patrick Wöhrer <patrickwhrer@gmail.com>
2023-10-21 19:54:09 +0200

    Display funktioniert

--- TemperatureAndHumidity.ino
+++ TemperatureAndHumidity.ino
@@ -1,9 +1,31 @@
 #include <SimpleDHT.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+const int c_screen_width = 128;    // OLED display width, in pixels
+const int c_screen_height = 32;    // OLED display height, in pixels
+const int c_screen_adress = 0x3C;  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
 SimpleDHT11 dht11(26);
 
+void u8g2_prepare() {
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.setFontRefHeightExtendedText();
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+}
+
 void setup() {
   Serial.begin(115200);
+
+  u8g2.begin();
+  u8g2_prepare();
 }
 
 void loop() {
@@ -11,7 +33,7 @@ void loop() {
   float humidty = 0;
   int err = SimpleDHTErrSuccess;
 
-  if((err=dht11.read2(&temperature, &humidty, NULL)) != SimpleDHTErrSuccess){
+  if ((err = dht11.read2(&temperature, &humidty, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err=");
     Serial.println(err);
     delay(2000);
@@ -22,6 +44,14 @@ void loop() {
   Serial.print("°C \n");
   Serial.printf("Humidity %.0f", humidty);
   Serial.printf("%%");
-  Serial.println(); 
+  Serial.println();
+
+  u8g2.clearBuffer();
+  u8g2_prepare();
+  u8g2.drawStr(0, 0, "Temperatur: ");
+  u8g2.drawStr(0, c_screen_height * 0.33, "Luftfeuchtigkeit: ");
+  u8g2.drawStr(0, c_screen_height * 0.66, "Aussentemperatur: ");
+  u8g2.sendBuffer();
+
   delay(5000);
 }
